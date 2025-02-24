45ebfdba56adff9dfb0db807609a31e27398cdaa
iianuragii <imanurag18@gmail.com>
2025-02-24 13:19:26 +0530

    Change in server deployed page

--- Backend/server.js
+++ Backend/server.js
@@ -11,6 +11,30 @@ app.use(express.json());
 
 app.use('/api', chatRoutes);
 
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <style>
+          body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            background-color: rgba(255, 237, 201, var(--tw-bg-opacity, 1));
+            font-size: 70;
+            font-family: Arial, sans-serif;
+          }
+        </style>
+      </head>
+      <body>
+        <div>This is the server for PujoQio</div>
+      </body>
+    </html>
+  `);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
